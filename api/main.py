1a53fc7b6d3c53f91cde0ccee194ecbef046cb02
JaoSchmidt <jhsc98@gmail.com>
2023-12-29 20:13:41 -0300

    fix

--- api/main.py
+++ api/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify, render_template
-# import mysql.connector
+import mysql.connector
 
 app = Flask(__name__)
 
 
 DATABASE_CONFIG = {
     "host":"db",
-    "user": "the_user",
-    "passowrd": "the_password",
+    "user": "test",
+    "password": "password",
     "database": "attestation"
 }
 
@@ -15,47 +15,47 @@ DATABASE_CONFIG = {
 def main():
     return render_template('./form.html')
 
-# @app.route('/submit_form', methods=['POST'])
-# def submit_form():
-#     try:
-#         data = request.get_json()
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    try:
+        data = request.get_json()
 
-#         product_name = data.get('product_name')
-#         version_number = data.get('version_number')
-#         responsible_person = data.get('responsible_person')
+        product_name = data.get('product_name')
+        version_number = data.get('version_number')
+        responsible_person = data.get('responsible_person')
 
-#         connection = mysql.connector.connect(**DATABASE_CONFIG)
-#         cursor = connection.cursor()
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = connection.cursor()
 
-#         # ... (perform database operations as in the previous example)
-#         print(product_name)
-#         print(version_number)
-#         print(responsible_person)
+        # ... (perform database operations as in the previous example)
+        print(product_name)
+        print(version_number)
+        print(responsible_person)
 
-#         connection.commit()
-#         connection.close()
+        connection.commit()
+        connection.close()
 
-#         return jsonify({"message": "Form submitted successfully!","product":product_name}), 200
+        return jsonify({"message": "Form submitted successfully!","product":product_name}), 200
 
-#     except Exception as e:
-#         return jsonify({"error": f"Error submitting form: {str(e)}"}), 500
+    except Exception as e:
+        return jsonify({"error": f"Error submitting form: {str(e)}"}), 500
 
-# @app.route('/get_form', methods=['GET'])
-# def get_form():
-#     try:
-#         connection = mysql.connector.connect(**DATABASE_CONFIG)
-#         cursor = connection.cursor()
+@app.route('/get_form', methods=['GET'])
+def get_form():
+    try:
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = connection.cursor()
 
-#         # Retrieve form data from the database
-#         cursor.execute("SELECT ProductName, VersionNumber, ResponsiblePerson FROM AttestationForm")
-#         forms = [{"product_name": row[0], "version_number": row[1], "responsible_person": row[2]} for row in cursor.fetchall()]
+        # Retrieve form data from the database
+        cursor.execute("SELECT ProductName, VersionNumber, ResponsiblePerson FROM AttestationForm")
+        forms = [{"product_name": row[0], "version_number": row[1], "responsible_person": row[2]} for row in cursor.fetchall()]
 
-#         connection.close()
+        connection.close()
 
-#         return jsonify({"forms": forms}), 200
+        return jsonify({"forms": forms}), 200
 
-#     except Exception as e:
-#         return jsonify({"error": f"Error retrieving form data: {str(e)}"}), 500
+    except Exception as e:
+        return jsonify({"error": f"Error retrieving form data: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)

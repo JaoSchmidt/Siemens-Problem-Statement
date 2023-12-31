29bc996e51c4cfe57ad732bfbf210eb8417ba156
sodalita2 <gabrielwb90@gmail.com>
2023-12-31 01:51:18 -0300

    Feat (AttestationForm): Sistema de incluir e tirar de sections pronto, falta server-side verification

--- api/main.py
+++ api/main.py
@@ -251,4 +251,4 @@ def get_form():
         return jsonify({"error": f"Error retrieving form data: {str(e)}"}), 500 """
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000,threaded=True)
